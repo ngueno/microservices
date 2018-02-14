b60e2b660e5e1a359092e9e198b7068b4bf68803
Norton T GUENO <nortontgueno@hotmail.com>
2018-02-13 23:28:00 -0200

    Adding examples of internationalization

--- restful-webservices/src/main/java/com/ngueno/rest/webservices/app/RestfulWebservicesApplication.java
+++ restful-webservices/src/main/java/com/ngueno/rest/webservices/app/RestfulWebservicesApplication.java
@@ -1,8 +1,15 @@
 package com.ngueno.rest.webservices.app;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.ngueno.rest.webservices" })
@@ -10,5 +17,19 @@ public class RestfulWebservicesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestfulWebservicesApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
+	}
+
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("i18n/messages");
+		return messageSource;
 	}
 }
--- restful-webservices/src/main/java/com/ngueno/rest/webservices/example/controller/HelloWorldController.java
+++ restful-webservices/src/main/java/com/ngueno/rest/webservices/example/controller/HelloWorldController.java
@@ -1,13 +1,21 @@
 package com.ngueno.rest.webservices.example.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ngueno.rest.webservices.example.bean.HelloWorldBean;
 
 @RestController
 public class HelloWorldController {
+
+	@Autowired
+	private MessageSource messageSource;
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
@@ -22,5 +30,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/path-variable/{name}")
 	public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s", name));
+	}
+
+	@GetMapping(path = "/hello-world-internationalized")
+	public String helloWorldInternationalized(
+			@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
 	}
 }
