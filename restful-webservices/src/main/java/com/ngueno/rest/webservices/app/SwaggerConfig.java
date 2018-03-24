1b7c3801b10b9a83b5176713353fda0403c830f3
Norton T GUENO <nortontgueno@hotmail.com>
2018-03-24 19:17:58 -0300

    Adding Swagger documentation and Postman environment

---
+++ restful-webservices/src/main/java/com/ngueno/rest/webservices/app/SwaggerConfig.java
@@ -0,0 +1,43 @@
+package com.ngueno.rest.webservices.app;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.VendorExtension;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author Norton Gueno
+ *
+ *         Simple Swagger configuration, after the application starts, please
+ *         check the following URLs:
+ * 
+ *         * http://localhost:8080/swagger-ui.html *
+ *         http://localhost:8080/v2/api-docs
+ *
+ */
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+	public static final Contact DEFAULT_CONTACT = new Contact("Norton Gueno", "https://github.com/ngueno/microservices","guenonorton@gmail.com");
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo("REST Training Documentation", "REST Docs", "1.0","urn:tos", DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0",new ArrayList<VendorExtension>());
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(Arrays.asList("application/json", "application/xml"));
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_AND_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+	}
+}
--- restful-webservices/src/main/java/com/ngueno/rest/webservices/app/entities/User.java
+++ restful-webservices/src/main/java/com/ngueno/rest/webservices/app/entities/User.java
@@ -1,22 +1,25 @@
 package com.ngueno.rest.webservices.app.entities;
 
 import java.util.Date;
-import java.util.List;
 
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(description="Details about the user")
 public class User {
 
 	private Long id;
 
 	@Past
+	@ApiModelProperty(notes = "Birth date should in the past")
 	private Date birtDate;
 
 	@Size(min = 2, message = "Name should have at least 2 characters")
+	@ApiModelProperty(notes = "Name should have at least 2 characters")
 	private String name;
-
-	private List<Post> posts;
 
 	public User() {
 
@@ -51,13 +54,5 @@ public class User {
 
 	public void setName(String name) {
 		this.name = name;
-	}
-
-	public List<Post> getPosts() {
-		return posts;
-	}
-
-	public void setPosts(List<Post> posts) {
-		this.posts = posts;
 	}
 }
