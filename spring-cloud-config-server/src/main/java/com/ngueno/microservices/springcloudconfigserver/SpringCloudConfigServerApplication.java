18c0df4bda4cd3126afd9969c11f29e53e7653b0
Norton T GUENO <nortontgueno@hotmail.com>
2018-09-15 21:08:05 -0300

    Integration with Spring Cloud Config

--- spring-cloud-config-server/src/main/java/com/ngueno/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
+++ spring-cloud-config-server/src/main/java/com/ngueno/microservices/springcloudconfigserver/SpringCloudConfigServerApplication.java
@@ -2,8 +2,10 @@ package com.ngueno.microservices.springcloudconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.ComponentScan;
 
+@EnableConfigServer
 @SpringBootApplication
 @ComponentScan(basePackages = { "com.ngueno" })
 public class SpringCloudConfigServerApplication {
