64fa394aa06afb26afc4e03ddcdbf7545a58e692
Norton T GUENO <nortontgueno@hotmail.com>
2018-09-23 18:34:05 -0300

    Setting up Zuul Gateway API

--- currency-conversion-service/src/main/java/com/ngueno/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
+++ currency-conversion-service/src/main/java/com/ngueno/microservices/currencyconversionservice/proxy/CurrencyExchangeServiceProxy.java
@@ -7,10 +7,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.ngueno.microservices.currencyconversionservice.bean.CurrencyConversionBean;
 
-@FeignClient(name = "currency-exchange-service")
+@FeignClient(name = "zuul-api-gateway-server")
 @RibbonClient(name = "currency-exchange-service")
 public interface CurrencyExchangeServiceProxy {
 
-	@GetMapping("/currency-exchange/from/{from}/to/{to}")
+	@GetMapping("/currency-exchange-service/currency-exchange/from/{from}/to/{to}")
 	public CurrencyConversionBean retrieveExchangeValue(@PathVariable("from") String from, @PathVariable("to") String to);
 }
