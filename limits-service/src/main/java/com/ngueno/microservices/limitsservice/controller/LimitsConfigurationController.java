49ef728bcd006191b37a737a1b0c19189e74f123
Norton T GUENO <nortontgueno@hotmail.com>
2018-09-29 19:10:45 -0300

    Implementing Hystrix for Fault Tolerance

--- limits-service/src/main/java/com/ngueno/microservices/limitsservice/controller/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/ngueno/microservices/limitsservice/controller/LimitsConfigurationController.java
@@ -1,13 +1,16 @@
 package com.ngueno.microservices.limitsservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.ngueno.microservices.limitsservice.bean.LimitConfiguration;
 import com.ngueno.microservices.limitsservice.config.Configuration;
 
 @RestController
+@EnableHystrix
 public class LimitsConfigurationController {
 
 	@Autowired
@@ -16,5 +19,15 @@ public class LimitsConfigurationController {
 	@GetMapping("/limits")
 	public LimitConfiguration retrieveLimitsConfiguration() {
 		return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+	}
+	
+	@GetMapping("/fault-tolerance-example")
+	@HystrixCommand(fallbackMethod="fallbackRetrieveConfiguration")
+	public LimitConfiguration retrieveConfiguration() {
+		throw new RuntimeException("Not available :)");
+	}
+	
+	public LimitConfiguration fallbackRetrieveConfiguration() {
+		return new LimitConfiguration(999, 9); 
 	}
 }
